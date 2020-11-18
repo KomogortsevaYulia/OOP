b051041f46f56269be3b01d0b5545bc8cdc0a0c6
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2020-11-18 20:27:26 +0800

    Update Main.java

--- 3 лаба/src/com/company/Main.java
+++ 3 лаба/src/com/company/Main.java
@@ -1,16 +1,7 @@
 package com.company;
 public class Main {
     public static void main(String[] args) {
-            Menu.menu();
+            Menu.mainMenu();
         }
 }
-        //код для тестирования
-        /*
-        Strings test1=new Strings();                       //использование конструктора  по умолчанию
-        Strings test2=new Strings("Это проверка");         //использование конструктора  с параметрами
-        Strings test3=new Strings(test1);                  //использование конструктора копирования
-        System.out.println("test1(адрес;содержимое): "+test1+" ; "+test1.getS());
-        System.out.println("test2(адрес;содержимое): "+test2+" ; "+test2.getS());
-        System.out.println("test3(адрес;содержимое): "+test3+" ; "+test3.getS());
-         */
 
