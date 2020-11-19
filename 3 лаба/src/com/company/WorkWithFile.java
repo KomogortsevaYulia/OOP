14a1cd8efc8817d1ef68514161a9843b802ffa9d
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2020-11-20 01:35:10 +0800

    Update WorkWithFile.java

--- 3 лаба/src/com/company/WorkWithFile.java
+++ 3 лаба/src/com/company/WorkWithFile.java
@@ -1,23 +1,30 @@
 package com.company;
-
 import java.io.*;
-
 public class WorkWithFile {
     //чтение из файла
     public static String strFromTxtFile(String filename){
-        String s=null;
+        String rez;
         try(BufferedReader br=new BufferedReader(new FileReader(filename))){
-            s=br.readLine();
+            StringBuffer sb = new StringBuffer();
+            rez=br.readLine();
+            for (int i=0;i<rez.length();i++){
+                sb.append(rez.charAt(i));
+            }
+             rez=sb.toString();
+        }
+        catch (NullPointerException e) {
+            System.out.println("Файл пустой");
+            rez=null;
         }
         catch (IOException e) {
             System.out.println("Ошибка чтения");
+            rez=null;
         }
-        return s;
+        return rez;
     }
 
     //запись в файл
     public static void strToTxtFile(String str, String filename){
-
         try(BufferedWriter br=new BufferedWriter(new FileWriter(filename))){
             br.write(str);
         }
