247cf5b27c8af10fb9163d747fd272870541136c
KomogortsevaYulia <74527737+KomogortsevaYulia@users.noreply.github.com>
2020-11-20 01:35:06 +0800

    Update Menu.java

--- 3 лаба/src/com/company/Menu.java
+++ 3 лаба/src/com/company/Menu.java
@@ -1,5 +1,4 @@
 package com.company;
-import java.io.*;
 import java.util.Scanner;
 
 public class Menu {
@@ -38,9 +37,114 @@ public class Menu {
                     System.out.println("Нет такого пункта");
             }
         } while (key1!=4);
+        System.out.println("До скорой встречи!");
     }
 
-    //проверка что бы было число
+    //делает задание 1
+    public static void  MakeTask1() {
+        //выводит меню ввода
+        printMenuInput();
+        //читает и проверят на число
+        int keyInput = InputInt();
+        //создаем обьект,необходимый для решения
+        Strings String1=new Strings();
+        switch (keyInput) {
+            case 1:
+                //ввод с консоли
+                System.out.print("Введите предложение:");
+                String1.setS(InputStr());
+                //вывод результата
+                OutputData(Strings.getResult1(String1.getS()));
+                break;
+            case 2:
+                //ввод с файла
+                //если файл не пустой то считать данные оттуда и найти и вывести результат
+                if(WorkWithFile.strFromTxtFile("Inputfile.txt")!=null){
+                    String1.setS(WorkWithFile.strFromTxtFile("Inputfile.txt"));
+                    OutputData(Strings.getResult1(String1.getS()));
+                }
+                break;
+            default:
+                System.out.println("Нет такого пункта");
+        }
+    }
+
+    //делает задание 2
+    public static void  MakeTask2() {
+        printMenuInput();
+        int keyInput = InputInt();
+        Strings String2=new Strings();
+        switch (keyInput) {
+            case 1:
+                //ввод с консоли
+                System.out.print("Введите предложение:");
+                String2.setS(InputStr());
+                //вывод результата
+                OutputData(Strings.getResult2(String2.getS()));
+                break;
+            case 2:
+                //ввод с файла
+                //если файл не пустой то считать данные оттуда и найти и вывести результат
+                if(WorkWithFile.strFromTxtFile("Inputfile.txt")!=null){
+                    String2.setS(WorkWithFile.strFromTxtFile("Inputfile.txt"));
+                    OutputData(Strings.getResult2(String2.getS()));
+                }
+                break;
+            default:
+                System.out.println("Нет такого пункта");
+        }
+    }
+
+    //делает задание 3
+    public static void  MakeTask3() {
+        printMenuInput();
+        int NumberWord=0;
+        Strings String3=new Strings();
+        int keyInput = InputInt();
+        switch (keyInput) {
+            case 1:
+                //ввод с консоли
+                System.out.print("Введите предложение:");
+                String3.setS(InputStr());
+                System.out.print("С длинной какого слова будем сравнивать?:");
+                //проверка на число
+                NumberWord=InputInt()-1;
+                //массив слов из файла
+                //если номер слова из файла больше чем количество слов или меньше
+                while  ((NumberWord> String3.getS().split(" ").length)||((NumberWord<0))){
+                    System.out.print("Слова с таким номером не существует,введите еще раз");
+                    NumberWord=InputInt()-1;
+                }
+                //вывод результата
+                OutputData(Strings.getResult3(String3.getS(), NumberWord));
+                break;
+            case 2:
+                //ввод с файла
+                //если файл не пустой то считать данные оттуда и найти и вывести результат
+                if(WorkWithFile.strFromTxtFile("Inputfile.txt")!=null) {
+                    String s = WorkWithFile.strFromTxtFile("Inputfile.txt");
+                    //в обьект записываем данные из файла до черты |
+                    String3.setS(s.substring(0, s.indexOf("|")));
+                    //массив из слов в предложение
+                    //длинна всей строки
+                    int t = s.length();
+                    //номер слова из файла
+                    NumberWord = Integer.parseInt(s.substring(s.indexOf("|") + 1, t)) - 1;
+                    //если номер слова из файла больше чем количество слов или меньше
+                    if ((NumberWord > String3.getS().split(" ").length) || (NumberWord < 0)) {
+                        OutputData("Слова с таким номером не существует");
+                    } else {
+                        //вывод результата
+                        OutputData(Strings.getResult3(String3.getS(), NumberWord));
+                    }
+                }
+                break;
+            default:
+                System.out.println("Нет такого пункта");
+        }
+    }
+
+    //метод для проверки что в консоли ввели число
     public static int InputInt(){
         int read ;
         try{
@@ -52,14 +156,28 @@ public class Menu {
         return read;
     }
 
+    //метод для проверки что в консоли ввели строку
+    public static String InputStr(){
+        String rez=null;
+        try{
+            rez=in.nextLine();
+            int i=1/rez.length();
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Вы ничего не ввели.Повторите ввод:");
+            InputStr();
+        }
+        return rez;
+    }
+
     //вывод результата
     public static void OutputData(String rezult) {
         System.out.println("Выберите, куда вывести результат:");
         System.out.println("1.В консоль;");
         System.out.println("2.В текстовый файл;");
         //опять ввели и проверили число ли это
-        int key=InputInt();
-        switch (key){
+        int keyOutput=InputInt();
+        switch (keyOutput){
             case 1:
                 //вывод в консоль
                 System.out.println(rezult);
@@ -81,138 +199,5 @@ public class Menu {
         System.out.println("1.Ввод с консоли;");
         System.out.println("2.Ввод из файла;");
 
-    }
-
-    //делает задание 1
-    public static void  MakeTask1() {
-        //выводит меню ввода
-        printMenuInput();
-        //читает и проверят на число
-        int keyInput = InputInt();
-        //создаем обьект,необходимый для решения
-        Strings String1=new Strings();
-        switch (keyInput) {
-            case 1:
-                //ввод с консоли
-                System.out.print("Введите предложение:");
-                String1.setS(in.nextLine());
-                //пока не введут значение будут запрашивать еще раз
-                while (String1.getS()==null){
-                    System.out.print("Пусто.Введите заново");
-                    String1.setS(in.nextLine());
-                }
-                //вывод результата
-                OutputData(Strings.getResult1(String1.getS()));
-                break;
-            case 2:
-                //ввод с файла
-                //если файл не пустой то считать данные оттуда и найти и вывести результат
-                if (WorkWithFile.strFromTxtFile("Inputfile.txt")!=null){
-                    String1.setS(WorkWithFile.strFromTxtFile("Inputfile.txt"));
-                    OutputData(Strings.getResult1(String1.getS()));
-                }
-                //иначе вывести в файл сообщение
-                else{
-                    OutputData("Файл пустой");
-                }
-                break;
-            default:
-                System.out.println("Нет такого пункта");
-        }
-    }
-
-    //делает задание 2
-    public static void  MakeTask2() {
-        printMenuInput();
-        int keyInput = InputInt();
-        Strings String2=new Strings();
-        switch (keyInput) {
-            case 1:
-                //ввод с консоли
-                System.out.print("Введите предложение:");
-                String2.setS(in.nextLine());
-                //пока не введут значение будут запрашивать еще раз
-                while (String2.getS()==null){
-                    System.out.print("Пусто.Введите заново");
-                    String2.setS(in.nextLine());
-                }
-                //вывод результата
-                OutputData(Strings.getResult2(String2.getS()));
-                break;
-            case 2:
-                //ввод с файла
-                //если файл не пустой то считать данные оттуда и найти и вывести результат
-                if (WorkWithFile.strFromTxtFile("Inputfile.txt")!=null){
-                    String2.setS(WorkWithFile.strFromTxtFile("Inputfile.txt"));
-                    OutputData(Strings.getResult2(String2.getS()));
-                }
-                //иначе вывести в файл сообщение
-                else{
-                    OutputData("Файл пустой");
-                }
-                break;
-            default:
-                System.out.println("Нет такого пункта");
-        }
-    }
-
-    //делает задание 3
-    public static void  MakeTask3() {
-        printMenuInput();
-        int NumberWord=0;
-        Strings String3=new Strings();
-        int keyInput = InputInt();
-        switch (keyInput) {
-            case 1:
-                //ввод с консоли
-                System.out.print("Введите предложение:");
-                String3.setS(in.nextLine());
-                //пока не введут значение будут запрашивать еще раз
-                while (String3.getS()==null){
-                    System.out.print("Пусто.Введите заново");
-                    String3.setS(in.nextLine());
-                }
-                System.out.print("С длинной какого слова будем сравнивать?:");
-                //проверка на число
-                NumberWord=InputInt();
-                //массив слов из файла
-                String[] ArrWord=String3.getS().split(" ");
-                //если номер слова из файла больше чем количество слов или меньше
-                while  ((NumberWord> ArrWord.length)||((NumberWord<0))){
-                    System.out.print("Слова с таким номером не существует,введите еще раз");
-                    NumberWord=InputInt();
-                }
-                //вывод результата
-                OutputData(Strings.getResult3(String3.getS(), NumberWord));
-                break;
-            case 2:
-                //ввод с файла
-                //если файл не пустой то считать данные оттуда и найти и вывести результат
-                if (WorkWithFile.strFromTxtFile("Inputfile.txt")!=null){
-                    String s=WorkWithFile.strFromTxtFile("Inputfile.txt");
-                    //в обьект записываем данные из файла до черты |
-                    String3.setS(s.substring(0,s.indexOf("|")));
-                    //массив из слов в предложение
-                    ArrWord=String3.getS().split(" ");
-                    //длинна всей строки
-                    int t= s.length();
-                    //номер слова из файла
-                    NumberWord=Integer.parseInt(s.substring(s.indexOf("|")+1,t))-1;
-                    //если номер слова из файла больше чем количество слов или меньше
-                    if ((NumberWord>ArrWord.length)||(NumberWord<0)){
-                        OutputData("Слова с таким номером не существует");
-                    }
-                    else{
-                        //вывод результата
-                        OutputData(Strings.getResult3(String3.getS(), NumberWord));
-                    }
-                }
-                else{
-                    OutputData("Файл пустой");
-                }
-                break;
-            default:
-                System.out.println("Нет такого пункта");
-        }
     }
 }
